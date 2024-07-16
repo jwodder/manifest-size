c46f492ea52fa8e7cd78d1c806ad29ea561edfa9
John T. Wodder II <git@varonathe.org>
2024-07-16 10:44:05 -0400

    Count raw bytes as they're read

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,52 @@ mod manifest;
 use crate::manifest::Manifest;
 use anyhow::Context;
 use get_size::GetSize;
-use std::io::Read;
+use std::io::{self, BufReader, Read};
 use thousands::Separable;
 
 fn main() -> anyhow::Result<()> {
     let Some(url) = std::env::args().nth(1) else {
         anyhow::bail!("No URL argument supplied");
     };
-    let mut r = ureq::get(&url)
-        .call()
-        .context("GET request failed")?
-        .into_reader();
-    let mut body = Vec::new();
-    r.read_to_end(&mut body)
-        .context("failed to read response body")?;
-    let body_len_str = body.len().separate_with_spaces();
-    let parsed =
-        serde_json::from_slice::<Manifest>(&body).context("failed to deserialize response")?;
+    let mut counter = CountingReader::new(
+        ureq::get(&url)
+            .call()
+            .context("GET request failed")?
+            .into_reader(),
+    );
+    let parsed = serde_json::from_reader::<_, Manifest>(BufReader::new(&mut counter))
+        .context("failed to deserialize response")?;
+    let body_len_str = counter.bytes_read().separate_with_spaces();
     let parsed_size_str = parsed.get_size().separate_with_spaces();
     let width = body_len_str.len().max(parsed_size_str.len());
     println!("Raw    response: {body_len_str:>width$} bytes");
     println!("Parsed response: {parsed_size_str:>width$} bytes");
     Ok(())
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct CountingReader<R> {
+    inner: R,
+    bytes_read: usize,
+}
+
+impl<R> CountingReader<R> {
+    fn new(inner: R) -> Self {
+        CountingReader {
+            inner,
+            bytes_read: 0,
+        }
+    }
+
+    fn bytes_read(&self) -> usize {
+        self.bytes_read
+    }
+}
+
+impl<R: Read> Read for CountingReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let qty = self.inner.read(buf)?;
+        self.bytes_read += qty;
+        Ok(qty)
+    }
 }
